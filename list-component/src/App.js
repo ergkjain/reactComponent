7b2f2c2cfc2e6e7d99fe6085fd4e604656c2909b
Gajendra Jain <er.gkjain@gmail.com>
2023-05-23 14:50:22 +0530

    initial commit for react list component

---
+++ list-component/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';  
+class App extends React.Component {  
+ constructor() {  
+      super();  
+      this.state = {  
+         data:   
+         [  
+            {             
+               "name":"Gajendra"             
+            },  
+            {            
+               "name":"Shantinath"             
+            },  
+            {    
+               "name":"Aadinath"          
+            }  
+         ]  
+      }  
+   }  
+   render() {  
+      return (  
+         <div>  
+            <StudentName/>  
+            <ul>            
+                {this.state.data.map((item) => <List data = {item} />)}           
+            </ul>  
+         </div>  
+      );  
+   }  
+}  
+class StudentName extends React.Component {  
+   render() {  
+      return (  
+         <div>  
+            <h1>Student Name Detail</h1>  
+         </div>  
+      );  
+   }  
+}  
+class List extends React.Component {  
+   render() {  
+      return (  
+         <ul>            
+            <li>{this.props.data.name}</li>   
+         </ul>  
+      );  
+   }  
+}  
+export default App;  
